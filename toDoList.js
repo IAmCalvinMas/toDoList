a0f83831446ecff99c1630d0566f1a65bf7b9fb5
iamcalvinmas <calvinmasza@gmail.com>
2022-12-10 15:41:42 +0200

    added README.md

---
+++ toDoList.js
@@ -0,0 +1,39 @@
+/*
+ * name: to-do list
+ * author: calvinmas@spacemilk
+ */
+
+
+
+/* 
+ 05:00 - 09:00
+   wake up
+   play music and exercise
+   go to run
+   bath and dress up
+   clean your room
+   eat breakfast
+   check emails and respond 
+
+
+ 09:00 - 13:00 
+   study mathematics
+
+
+ 13:00 - 17:00
+   eat lunch
+   maths tutoring
+   snooker
+
+
+ 17:00 - 20:30
+   supper
+   bath
+   exercise
+   prepare for tomorrow
+   read bible & pray
+
+
+ 21:00 - 05:00 
+   sleep
+*/
